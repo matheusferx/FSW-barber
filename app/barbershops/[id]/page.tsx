80fff35a5d62f24046e4ba0a2ba402f56de5c80a
Matheus Fernandes <matheusfernandes1028@gmail.com>
2025-09-14 21:35:04 -0300

    chore: add project base structure

---
+++ app/barbershops/[id]/page.tsx
@@ -0,0 +1,92 @@
+import PhoneItem from "@/app/_components/phone-item";
+import ServiceItem from "@/app/_components/service-item";
+import { Button } from "@/app/_components/ui/button";
+import { db } from "@/app/_lib/prisma"
+import { ChevronLeftIcon, MapPinIcon, MenuIcon, StarIcon } from "lucide-react";
+import Image from "next/image";
+import Link from "next/link";
+import { notFound } from "next/navigation";
+
+interface BarbershopPageProps {
+    params: {
+        id: string
+    }
+}
+
+const BarbershopPage = async ({params}: BarbershopPageProps) => {
+    // Chamar o banco de dados
+
+    const barbershop = await db.barbershop.findUnique({
+        where: {
+            id: params.id
+        },
+        include: {
+            services: true,
+        },
+    });
+
+    if (!barbershop) {
+        return notFound()
+    }
+
+  return (
+    <div>
+        {/* IMAGEM */}
+        <div className="relative h-[250px] w-full">
+            <Image alt={barbershop.name} src={barbershop.imageUrl} fill className="object-cover"/>
+
+            <Button size="icon" variant="secondary" className="absolute left-4 top-4" asChild >
+                <Link href="/" >
+                    <ChevronLeftIcon />
+                </Link>
+            </Button>
+
+            <Button size="icon" variant="secondary" className="absolute right-4 top-4" >
+                <MenuIcon />
+            </Button>
+        </div>
+
+        {/* TÍTULO */}
+        <div className="border-b border-solid p-5">
+            <h1 className="mb-3 text-xl font-bold">{barbershop.name}</h1>
+
+            <div className="mb-2 flex items-center gap-2">
+                <MapPinIcon className="text-primary" size={18} />
+                <p className="text-sm">{barbershop.address}</p>
+            </div>
+            
+            <div className="flex items-center gap-2">
+                <StarIcon className="fill-primary text-primary" size={18} />
+                <p className="text-sm">5.0 (499 avaliações)</p>
+            </div>
+        </div>
+
+        {/* DESCRIÇÃO */}
+        <div className="space-y-2 border-b border-solid p-5">
+            <h2 className="text-xs font-bold uppercase text-gray-400">Sobre nós</h2>
+            <p className="text-justify text-sm">{barbershop.description}</p>
+        </div>
+
+        {/* SERVIÇOS */}
+        <div className="p-5 space-y-3 border-b border-solid">
+            <h2 className="text-xs font-bold uppercase text-gray-400">Serviços</h2>
+            
+            <div className="space-y-3">
+                {barbershop.services.map((service) => (
+                    <ServiceItem key={service.id} service={service} />
+                ))}
+            </div>
+            
+        </div>
+
+        {/* CONTATO */}
+        <div className="p-5 space-y-3">
+            {barbershop.phones.map(phone => (
+                <PhoneItem phone={phone} />
+            ))}
+        </div>
+    </div>
+  )
+}
+
+export default BarbershopPage;
--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,9 @@
 import type { Metadata } from "next";
 import localFont from "next/font/local";
 import "./globals.css";
+import { Toaster } from "./_components/ui/sonner";
+import { Card, CardContent } from "./_components/ui/card";
+import Footer from "./_components/footer";
 
 const geistSans = localFont({
   src: "./fonts/GeistVF.woff",
@@ -24,11 +27,15 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
+    <html lang="en" className="dark">
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
         {children}
+
+        <Toaster />
+        <Footer />
+        
       </body>
     </html>
   );
